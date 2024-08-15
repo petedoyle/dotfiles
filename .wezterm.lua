fbb7804766697e464978300fada5fd4d20f40dc2
Pete Doyle <petedoyle@gmail.com>
2024-08-15 16:29:52 -0700

    Nest config inside a 'config' block

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,25 +1,26 @@
 local wezterm = require 'wezterm'
-local config = wezterm.config_builder()
 
---config.color_scheme = 'MaterialOcean'
-config.color_scheme = 'Modus-Vivendi-Tinted'
---config.color_scheme = 'catppuccin-mocha'
---config.color_scheme = 'Chalk (base16)'
---config.color_scheme = 'Chalk (dark) (terminal.sexy)'
---config.color_scheme = 'iTerm2 Default'
-config.font = wezterm.font('Monaco')
-config.font_size = 13.0
+config = wezterm.config_builder()
 
-config.visual_bell = {
-  fade_in_function = 'EaseIn',
-  fade_in_duration_ms = 150,
-  fade_out_function = 'EaseOut',
-  fade_out_duration_ms = 150,
+config = {
+  -- color_scheme = 'MaterialOcean',
+  color_scheme = 'Modus-Vivendi-Tinted',
+  -- color_scheme = 'catppuccin-mocha',
+  -- color_scheme = 'Chalk (base16)',
+  -- color_scheme = 'Chalk (dark) (terminal.sexy)',
+  -- color_scheme = 'iTerm2 Default',
+  font = wezterm.font('Monaco'),
+  font_size = 13.0,
+  visual_bell = {
+    fade_in_function = 'EaseIn',
+    fade_in_duration_ms = 150,
+    fade_out_function = 'EaseOut',
+    fade_out_duration_ms = 150,
+  },
+  colors = {
+    visual_bell = '#202020',
+  },
+  enable_scroll_bar = true,
 }
-config.colors = {
-  visual_bell = '#202020',
-}
-
-config.enable_scroll_bar = true
 
 return config
