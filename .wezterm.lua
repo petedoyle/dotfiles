dca8af6a61d8ec2170266a4a0906a4ea43016a9d
Pete Doyle <petedoyle@gmail.com>
2024-08-15 13:47:35 -0700

    WezTerm: Enable Visual Bell

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,5 +11,16 @@ config.color_scheme = 'AdventureTime'
 config.font = wezterm.font('Monaco')
 config.font_size = 12.0
 
+config.visual_bell = {
+  fade_in_function = 'EaseIn',
+  fade_in_duration_ms = 150,
+  fade_out_function = 'EaseOut',
+  fade_out_duration_ms = 150,
+}
+config.colors = {
+  visual_bell = '#202020',
+}
+
+
 -- and finally, return the configuration to wezterm
 return config
